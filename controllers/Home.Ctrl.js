066868c2f4fcb17b909e91a50e8f21fbb868df7d
Romain <romain@cozycloud.cc>
2016-07-11 15:23:55 +0200

    [Angular Step 2] Add cozysdk and list contacts

--- controllers/Home.Ctrl.js
+++ controllers/Home.Ctrl.js
@@ -1,8 +1,18 @@
 angular.module('browserapp').controller('HomeAngCtrl', HomeAngCtrl);
 
+HomeAngCtrl.$inject = ['CozySdk'];
+
 function HomeAngCtrl(CozySdk) {
     var vm = this;
 
-    vm.hello = "world";
-
+    CozySdk.defineRequest('Contact', 'all', 'function(doc) { emit(doc.n); }')
+    .then(function () {
+      return CozySdk.runRequest('Contact', 'all')
+    })
+    .then(function(res) {
+      vm.contacts = res;
+    })
+    .catch(function(error) {
+        vm.error = error;
+    });
 }
